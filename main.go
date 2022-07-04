9104c0ba2f87099cb5db8c0e95f247eaad28e507
Flávio Bianchetti <fjb.developer@gmail.com>
2022-07-04 08:10:52 -0300

    feat: connection configuration in main file

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/flavio-bianchetti/go-todo-api/controllers"
+	models "github.com/flavio-bianchetti/go-todo-api/models"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 	mux := controllers.Register()
-	http.ListenAndServe(":3000", mux)
+	db := models.Connect()
+	defer db.Close()
+	fmt.Println("Serving...")
+	log.Fatal(http.ListenAndServe(":3000", mux))
 }
