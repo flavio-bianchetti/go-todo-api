bc6af8bf491debbbf67a5bb906e902494b6425bf
Flávio Bianchetti <fjb.developer@gmail.com>
2022-07-05 09:00:40 -0300

    feat: ReadAll models method included

---
+++ models/reads.go
@@ -0,0 +1,20 @@
+package models
+
+import (
+	"github.com/flavio-bianchetti/go-todo-api/views"
+)
+
+func ReadAll() ([]views.GetRequest, error) {
+	rows, err := con.Query("SELECT * FROM todo")
+	if err != nil {
+		return nil, err
+	}
+	todos := []views.GetRequest{}
+	for rows.Next() {
+		data := views.GetRequest{}
+		rows.Scan(&data.Id, &data.Name, &data.Description)
+		todos = append(todos, data)
+	}
+	return todos, nil
+}
+
