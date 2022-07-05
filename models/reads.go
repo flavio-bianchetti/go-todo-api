06fc8440dd60e004bf413da1e934ccae78243f38
Flávio Bianchetti <fjb.developer@gmail.com>
2022-07-05 09:01:16 -0300

    feat: ReadByName model method included

--- models/reads.go
+++ models/reads.go
@@ -18,3 +18,16 @@ func ReadAll() ([]views.GetRequest, error) {
 	return todos, nil
 }
 
+func ReadByName(name string) ([]views.GetRequest, error) {
+	rows, err := con.Query("SELECT * FROM todo WHERE name=?", name)
+	if err != nil {
+		return nil, err
+	}
+	todos := []views.GetRequest{}
+	for rows.Next() {
+		data := views.GetRequest{}
+		rows.Scan(&data.Id, &data.Name, &data.Description)
+		todos = append(todos, data)
+	}
+	return todos, nil
+}
